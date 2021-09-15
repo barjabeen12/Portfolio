7def1f341ae7ba649b1000f00f7671ae4dd84b2d
barjabeen12 <barjabeen.16@gmail.com>
2021-09-15 12:38:21 +0500

    final push

---
+++ src/containers/profile/Profile.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect, lazy, Suspense} from "react";
+import {openSource} from "../../portfolio";
+import Contact from "../contact/Contact";
+import Loading from "../loading/Loading";
+
+const renderLoader = () => <Loading />;
+
+export default function Profile() {
+  const [prof, setrepo] = useState([]);
+  function setProfileFunction(array) {
+    setrepo(array);
+  }
+
+  useEffect(() => {
+    if (openSource.showGithubProfile === "true") {
+      const getProfileData = () => {
+        fetch("/profile.json")
+          .then(result => {
+            if (result.ok) {
+              return result.json();
+            }
+            console.error(result);
+          })
+          .then(response => {
+            setProfileFunction(response.data.user);
+          })
+          .catch(function (error) {
+            setProfileFunction("Error");
+            console.log(
+              "Because of this error, contact section has reverted to default"
+            );
+            console.error(error);
+            openSource.showGithubProfile = "false";
+          });
+      };
+      getProfileData();
+    }
+  }, []);
+  return <Contact />;
+  }
+
---
+++ src/containers/projects/Projects.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect, useContext, Suspense, lazy} from "react";
+import "./Project.scss";
+import Button from "../../components/button/Button";
+import {openSource, socialMediaLinks} from "../../portfolio";
+import StyleContext from "../../contexts/StyleContext";
+import Loading from "../../containers/loading/Loading";
+export default function Projects() {
+  const FailedLoading = () => null;
+  const renderLoader = () => <Loading />;
+  const [repo, setrepo] = useState([]);
+  // todo: remove useContex because is not supported
+  const {isDark} = useContext(StyleContext);
+
+  useEffect(() => {
+    
+  }, []);
+
+  function setrepoFunction(array) {
+    setrepo(array);
+  }
+  
+    return <FailedLoading />;
+  }
+
