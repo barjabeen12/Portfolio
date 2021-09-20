7357b19565bb52fdd842a4cc815885925d3d5bd8
barjabeen12 <barjabeen12@users.noreply.github.com>
2021-09-20 12:16:30 +0000

    Deploying to gh-pages from  @ 7ed00e18c4b5281f4b543b690398ad5b6f6a8b34 🚀

---
+++ precache-manifest.a73eead055d3bc8ca38c10f5f48646d9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab83fab4d28fd6964be03e8e46282907",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693218800dbbd51068bb",
+    "url": "/static/css/main.1fa0e935.chunk.css"
+  },
+  {
+    "revision": "3725ecacde17dc26ac49",
+    "url": "/static/js/2.87632149.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.87632149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693218800dbbd51068bb",
+    "url": "/static/js/main.654c9054.chunk.js"
+  },
+  {
+    "revision": "c58f1ea9baa43354727f",
+    "url": "/static/js/runtime-main.665abb56.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "337f27433c0dbf0481a06859f4927ca8",
+    "url": "/static/media/Udacity.337f2743.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "8f284adc6c67f73af27f390c53ac4058",
+    "url": "/static/media/numl.8f284adc.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
